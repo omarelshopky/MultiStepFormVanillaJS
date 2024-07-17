cffdeb60f9d7d90b8393e35d643be32b382f7bcd
Omar Elshopky <omar.elshopky202@gmail.com>
2024-07-17 05:09:57 +0300

    Quotation form step 1 validation, and submit logic

---
+++ js/quotation-form.js
@@ -0,0 +1,203 @@
+"use strict";
+
+
+/**
+ * Constants
+ */
+const QUOTATION_FORM_STEPS_COUNT = 5;
+const QUOTATION_FORM_STEPS = [
+    [
+        {
+            "name" : "height",
+            "type" : "float",
+            "min"  : 40,
+            "max"  : 400
+        },
+        {
+            "name" : "drawers",
+            "type" : "integer",
+            "min"  : 1,
+            "max"  : 8
+        },
+    ],
+    [
+
+    ]
+]
+
+/**
+ * Variables
+ */
+const quotationData = {};
+let quotationForm = {};
+let currentStep = 1;
+let validForm = false;
+let formButton = document.querySelector(".form-button");
+
+/**
+ * Functions
+ */
+const attachValidationEvents = () => {
+    switch (currentStep) {
+        case 1:
+            QUOTATION_FORM_STEPS[currentStep - 1].forEach(formInputData => {
+                const formInput = quotationForm[formInputData.name].input;
+                formInput.addEventListener("keyup", () => __validateInput(formInput.value, formInputData));
+            });
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
+}
+
+const getQuotationFormInput = () => {
+    quotationForm = {};
+
+    QUOTATION_FORM_STEPS[currentStep - 1].forEach(formInputData => {
+        quotationForm[formInputData.name] = {
+            "input" : document.querySelector(`.form-input__${formInputData.name}`),
+            "inputError" : document.querySelector(`.form-input-error__${formInputData.name}`),
+            "valid" : false,
+            "value" : null
+        };
+    });
+}
+
+const submitForm = () => {
+    __toggleFormButton();
+
+    if (!validForm) {
+        return;
+    }
+
+    Object.keys(quotationForm).forEach(inputName => {
+        quotationData[inputName] = quotationForm[inputName].value;
+    });
+
+    if (currentStep < QUOTATION_FORM_STEPS_COUNT) {
+        __getNextStep();
+    }
+}
+
+const __getNextStep = async () => {
+    currentStep++;
+    const formStepWrapper = document.querySelector(".form-step-wrapper");
+    formStepWrapper.innerHTML = "";
+    formStepWrapper.setAttribute(INCLUDE_HTML_ATTRIBUTE, `components/quotation-form-step-${currentStep}.html`);
+    await includeHTML();
+    getQuotationFormInput();
+    attachValidationEvents();
+}
+
+const __validateForm = () => {
+    switch (currentStep) {
+        case 1:
+            QUOTATION_FORM_STEPS[currentStep - 1].forEach(formInputData => {
+                __validateInput(quotationForm[formInputData.name].input.value, formInputData);
+            });
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
+}
+
+const __validateInput = (value, data) => {
+    const { name, type, min, max } = data;
+
+    if (quotationForm[name].value == value) {
+        return quotationForm[name].valid;
+    }
+
+    quotationForm[name].value = value;
+
+    switch (type) {
+        case "float":
+            return __validateFloatInput(name, value, min, max);
+        case "integer":
+            return __validateIntegerInput(name, value, min, max);
+    }
+}
+
+const __validateIntegerInput = (name, value, min, max) => {
+    value = parseFloat(value);
+
+    if (!Number.isInteger(value)) {
+        quotationForm[name].valid = false;
+        __setInputError(name, "Please enter a valid integer.");
+        __toggleFormButton();
+
+        return false;
+    }
+
+    if (value < min || value > max) {
+        quotationForm[name].valid = false;
+        __setInputError(name, `Please enter an integer between ${min} and ${max}.`);
+        __toggleFormButton();
+
+        return false;
+    }
+
+    quotationForm[name].valid = true;
+    __setInputError(name, "");
+    __toggleFormButton();
+
+    return true;
+}
+
+const __validateFloatInput = (name, value, min, max) => {
+    value = parseFloat(value);
+
+    if (Number.isNaN(value)) {
+        quotationForm[name].valid = false;
+        __setInputError(name, "Please enter a valid number.");
+        __toggleFormButton();
+
+        return false;
+    }
+
+    if (value < min || value > max) {
+        quotationForm[name].valid = false;
+        __setInputError(name, `Please enter a number between ${min} and ${max}.`);
+        __toggleFormButton();
+
+        return false;
+    }
+
+    quotationForm[name].valid = true;
+    __setInputError(name, "");
+    __toggleFormButton();
+
+    return true;
+}
+
+const __setInputError = (name, error) => {
+    quotationForm[name].inputError.innerHTML = error;
+}
+
+const __toggleFormButton = () => {
+    __checkFormInputsValidation();
+    formButton.disabled = !validForm;
+}
+
+const __checkFormInputsValidation = () => {
+    validForm = true;
+
+    Object.keys(quotationForm).forEach(formInputName => {
+        validForm &= quotationForm[formInputName].valid;
+    });
+}
+
+/**
+ * Initialization
+*/
+getQuotationFormInput();
+formButton.addEventListener("click", submitForm);
+attachValidationEvents();
